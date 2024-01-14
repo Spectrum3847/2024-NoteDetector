df22b2ae35991eaafc11152d8ae796aae0bcb7dc
Daniel-J101 <spectrum3847@gmail.com>
2024-01-13 18:48:10 -0600

    Deleted necessary file

--- compiler.py
+++
@@ -1,7 +0,0 @@
-populator_dst = metadata.MetadataPopulator.with_model_file('notedetector_edgetpu.tflite')
-
-with open('notedetector.tflite', 'rb') as f:
-  populator_dst.load_metadata_and_associated_files(f.read())
-
-populator_dst.populate()
-updated_model_buf = populator_dst.get_model_buffer()
